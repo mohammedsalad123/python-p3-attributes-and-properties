f1f1702875b46f991e91d91d24e083ea83f94ddb
mohammed <mohammed.salad@student.moringaschool.com>
2023-12-12 10:28:36 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,62 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+# class Dog:
+#     def __init__(self) :
+#         self._name = None
+#     def name(self):
+#         return self._name
+#     def name(self, value):
+#         if isinstance(value, str) and 1 <= len(value) <= 25:
+#             self._name = value
+#          else:
+#            print("Name must be a string between 1 and 25 characters.")
+    
+# my_dog = Dog()
+# my_dog.name = "relax"
+# print(my_dog.name)
+# my_dog.name="ThisIsAVeryLongNameForADog111111111111" 
+# print(my_dog.name)
+
 class Dog:
-    pass
+    def __init__(self):
+        self._name = None  # Private attribute to store the name
+        self._breed = None
+
+
+    def breed (self):
+        return self._breed
+    
+    def breed (self ,value):
+         if value in self.approved_breeds:
+            self._breed = value
+         else : 
+            print( " The breed must be in the following list of dog breeds")
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+            return  # Exit the setter without setting the name
+    
+# Creating a Dog object
+my_dog = Dog()
+
+# Setting the name using the property
+my_dog.name = "Buddy"  # Valid name
+my_dog.breed = "xxxx"
+print(my_dog.breed)
+# Accessing the name using the property
+print(my_dog.name)  # Output: Buddy
+
+# Trying to set an invalid name
+my_dog.name = "ThisIsAVeryLongNameForADog"  # Invalid name
+# Output: Name must be a string between 1 and 25 characters.
+
+# Checking the name again to confirm it wasn't changed due to an invalid input
+print(my_dog.name)  # Output: Buddy
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +14,41 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self):
+        self._name = None  # Private attribute to store the name
+        self._job = None   # Private attribute to store the job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and len(new_name) < 25:
+            self._name = new_name.title()
+        else:
+            print("Name must be a string under 25 characters.")
+            self._name = None  # Setting name to None if invalid
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+# Example usage:
+person = Person()
+
+person.name = "john doe"  # Valid name
+print(person.name)  # Output: John Doe
+
+person.name = "this is a very long name for a person"  # Invalid name
+# Output: Name must be a string under 25 characters.
+
+person.job = "vdvdvdvd"
+print(person.job)  # Output: Job must be in list of approved jobs.
